887ee45c12fed5d0332215eb3738e74958781afa
Rubio García <osrubio@minsait.com>
2024-09-19 10:18:44 +0200

    Included code

---
+++ src/test/java/osrubio/ej7/Ej7ApplicationTests.java
@@ -0,0 +1,13 @@
+package osrubio.ej7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
